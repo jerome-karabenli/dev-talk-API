bbd2fedfa3b29e0e379eea603fd87a01cd403598
Jerome KARABENLI <jkarabenli.dev@gmail.com>
2022-03-03 17:12:06 +0100

    fix compose file network

--- docs/swagger/servers.js
+++ docs/swagger/servers.js
@@ -1,7 +1,7 @@
 module.exports = {
     servers: [
       {
-        url: process.env.API_DOCS_SERVER, // url
+        url: `${process.env.API_DOCS_SERVER}${API_URL_PREFIX}`, // url
         description: "Demo server", // name
       },
     ],
